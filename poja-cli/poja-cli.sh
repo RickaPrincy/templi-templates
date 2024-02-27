9711d85966a1bafd49c0e579ed3c659901656077
RickaPrincy <rckprincy@gmail.com>
2024-02-27 09:51:57 +0300

    feat: add simple poja-cli sh gen

---
+++ poja-cli/poja-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python -m poja \
+  --app-name={{APP_NAME}} \
+  --package-full-name={{PACKAGE_FULL_NAME}} \
+  --region={{AWS_REGION}} \
+  --with-own-vpc=true \
+  --ssm-sg-id={{SG_ID}} \
+  --ssm-subnet1-id={{SUBNET1_ID}}\
+  --ssm-subnet2-id={{SUBNET2_ID}}
